80bc0d7467415d255d08fd167ab0fd199314fb58
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-05-07 08:00:29 +0100

    Add deposit method and deploy contract to localhost

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,30 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
+const {provider} = waffle;
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    it("Should return the new greeting once it's changed", async function () {
+        const Greeter = await ethers.getContractFactory("Greeter");
+        const greeter = await Greeter.deploy("Hello, world!");
+        await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+        expect(await greeter.greet()).to.equal("Hello, world!");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+        const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+        // wait until the transaction is mined
+        await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+        expect(await greeter.greet()).to.equal("Hola, mundo!");
+    });
+
+    it('should return the new balance after ether is deposited', async function () {
+        const Greeter = await ethers.getContractFactory("Greeter");
+        const greeter = await Greeter.deploy("Hello, World!");
+        await greeter.deployed();
+
+        await greeter.deposit({value: 10});
+        expect(await provider.getBalance(greeter.address)).to.equal(10);
+    });
+
 });
